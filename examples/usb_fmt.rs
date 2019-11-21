078557d916d4de40c95917b9a595983f0833aebc
Hanno Braun <hanno@braun-embedded.com>
2019-11-21 08:41:08 +0100

    Update dependencies

--- examples/usb_fmt.rs
+++ examples/usb_fmt.rs
@@ -36,13 +36,13 @@ use catena_4610::hal::{
     rcc,
     syscfg::SYSCFG,
     timer::Timer,
-    usb,
+    usb::{
+        USB,
+        UsbBusType,
+    },
 };
 use nb::block;
-use stm32_usbd::{
-    UsbBus,
-    UsbBusType,
-};
+use stm32_usbd::UsbBus;
 use usbd_serial::{
     SerialPort,
     USB_CLASS_CDC,
@@ -77,8 +77,7 @@ fn main() -> ! {
     let mut scb    = cp.SCB;
     let mut rcc    = dp.RCC.freeze(rcc::Config::hsi16());
     let mut pwr    = PWR::new(dp.PWR, &mut rcc);
-    let mut syscfg = SYSCFG::new(dp.SYSCFG_COMP, &mut rcc);
-    let     usb    = dp.USB;
+    let mut syscfg = SYSCFG::new(dp.SYSCFG, &mut rcc);
     let     gpioa  = dp.GPIOA.split(&mut rcc);
     let     gpiob  = dp.GPIOB.split(&mut rcc);
 
@@ -96,17 +95,18 @@ fn main() -> ! {
         LED = Some(gpiob.pb2.into_push_pull_output());
     }
 
-    usb::init(&mut rcc, &mut syscfg, dp.CRS);
-
     let usb_dm = gpioa.pa11;
     let usb_dp = gpioa.pa12;
+
+    let hsi48 = rcc.enable_hsi48(&mut syscfg, dp.CRS);
+    let usb   = USB::new(dp.USB, usb_dm, usb_dp, hsi48);
 
     // `UsbWriter` requires the USB bus to have a static lifetime. We're using
     // the `singleton` macro from `cortex-m` here to achieve this.
     let bus =
         singleton!(
             : UsbBusAllocator<UsbBusType> =
-                UsbBus::new(usb, (usb_dm, usb_dp))
+                UsbBus::new(usb)
         )
         .unwrap(); // never panics, as `main` is only called once
 
